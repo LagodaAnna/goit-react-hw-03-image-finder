06adabee4a7ce2c721af4e8376fbba3c58b9e874
Anna <lagoda1990@icloud.com>
2022-07-08 22:45:10 +0300

    Update ImageGallery.styled.js

--- src/components/ImageGallery/ImageGallery.styled.js
+++ src/components/ImageGallery/ImageGallery.styled.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const Container = styled.ul`
   display: grid;
   max-width: calc(100vw - 48px);
-  grid-template-columns: repeat(auto-fill, minmax(280px, 1fr));
+  grid-template-columns: repeat(auto-fill, minmax(320px, 1fr));
   grid-gap: 16px;
   margin-top: 0;
   margin-bottom: 0;
@@ -21,7 +21,7 @@ export const ImageContainer = styled.li`
 
 export const Image = styled.img`
   width: 100%;
-  height: 190px;
+  height: 260px;
   object-fit: cover;
   transition: transform 250ms cubic-bezier(0.4, 0, 0.2, 1);
 
